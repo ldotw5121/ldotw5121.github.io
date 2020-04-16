e363ba60695fbb52daabec54974bfd3a134ca2f2
ldotw5121 <62733730+ldotw5121@users.noreply.github.com>
2020-04-17 01:42:12 +0900

    site plus

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "모여봐요 동물의 숲",
+        "excerpt":"게임 정보 제작 NINTENDO 유통 NINTENDO 장르 시뮬레이션 등급 전체이용가 언어 한국어/영어/일본어 플랫폼 NINTENDO SWITCH 인원 로컬 최대 4인 / 온라인 최대 8인 발매 2020.03.20 공식 모여봐요 동물의 숲 공식 홈페이지 정가 64,800 KRW 전작 튀어나와요 동물의 숲 캐치프레이즈 何もないから、何でもできる 아무것도 없기에 무엇이든 할 수 있는 곳 유행 ​ 기존...","categories": ["Games"],
+        "tags": [],
+        "url": "http://localhost:4000/games/AnimalCrossing/",
+        "teaser": null
+      },{
+        "title": "Java",
+        "excerpt":"JDK 1.0a 1994년 발표. JDK1.0a2 1995년 5월 23일 발표. JDK 1.0 1996년 1월 23일 발표. 발표 이전에 불렸던 이름은 Oak였으며, 안정화 작업을 거친 1.0.2 버전에서 Java로 이름이 바뀌었다. JDK 1.1 1997년 2월 19일 발표. 이너 클래스, JavaBeans, RMI, 리플렉션, 유니코드 지원, 국제화(Internationalization) 등이 추가되었다. J2SE 1.2 1998년 12월 8일 발표....","categories": ["Assignment"],
+        "tags": [],
+        "url": "http://localhost:4000/assignment/java/",
+        "teaser": null
+      },{
+        "title": "슈트라센 알고리즘",
+        "excerpt":"슈트라센 알고리즘 기본적인 개념에 관해서는 여기에서 확인할 수 있다. 내용을 요약해보면 슈트라센 알고리즘을 활용해 행렬곱을 계산하는 시간이 일반적으로 우리가 알고있는 행렬곱을 처리하는 시간보다 빨라진다는 것이다. 슈트라센 알고리즘의 증명 일반적인 이차원 행렬의 곱셈을 확인해보자. 슈트라센 알고리즘에서는 M행렬을 새로 정의하여 사용한다. 이제 $C_{11}$을 풀어보면 이고, 식에서 같은 부분을 소거하고나면 슈트라센 알고리즘에 대한...","categories": ["Assignment"],
+        "tags": [],
+        "url": "http://localhost:4000/assignment/strassen/",
+        "teaser": null
+      },{
+        "title": "그리디 알고리즘",
+        "excerpt":"그리디 알고리즘 그리디 알고리즘에 관한 설명은 여기에서 확인할 수 있다. 교재에 나와있는 문제로는 동전 거스름돈 최소 신장 트리 최단 경로 찾기 부분 배낭 문제 집합 커버 문제 작업 스케줄링 하프만 압축 총 7가지 문제가 수록되어있다. 나는 이 그리디 알고리즘을 활용할 수 있는 문제를 백준에서 찾을 수 있었다. ATM 문제 인하은행에는...","categories": ["Assignment"],
+        "tags": [],
+        "url": "http://localhost:4000/assignment/Greedy/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["ETC"],
+        "tags": [],
+        "url": "http://localhost:4000/etc/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Banana",
+        "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": ["ETC"],
+        "tags": [],
+        "url": "http://localhost:4000/etc/Banana/",
+        "teaser": null
+      }]
